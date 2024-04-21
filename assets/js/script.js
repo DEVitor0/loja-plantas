0e218c6b5897ca167d2d8761dd38024d498b79b2
DEVitor0 <vitormoreira6940@gmail.com>
2024-04-20 21:57:26 -0300

    criação da animação com js para o texto de inicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,31 @@ function abreMenu() {
     const exibir = document.querySelectorAll('.container-abertura');
     const abertura = document.querySelectorAll('.abertura');
     
-    abertura.forEach((element1, index) => {
-        element1.addEventListener('click', function () {
+    abertura.forEach((element, index) => {
+        element.addEventListener('click', function () {
             const display = exibir[index].style.display;
             exibir[index].style.display = (display === 'flex') ? 'none' : 'flex';
         });
     });
 }
-
 abreMenu();
+
+function animaçãoInicial() {
+    const containerTitulo = document.querySelector("#titulo-carrossel");
+    const meuObservador = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                containerTitulo.classList.add('slideAnimation');
+            } else {
+                containerTitulo.classList.remove('slideAnimation');
+            }
+        });
+    });
+    meuObservador.observe(containerTitulo); 
+}
+
+animaçãoInicial();
+
+
+
 
