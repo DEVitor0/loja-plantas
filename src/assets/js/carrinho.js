5326c75b5041319cb8f2a32a666134913499724b
DEVitor0 <vitormoreira6940@gmail.com>
2024-04-27 17:17:46 -0300

    salvando as novas alterações

--- src/assets/js/carrinho.js
+++ src/assets/js/carrinho.js
@@ -1,4 +1,4 @@
-import { removeElemento, displayNone, mensagemTela } from "../../../public/assets/js/script";
+import { displayNone } from "./script";
 
 function abreMenu() {
     const iconeCarrinho = window.document.querySelector('#abrir-carrinho');
@@ -7,7 +7,6 @@ function abreMenu() {
     iconeCarrinho.addEventListener('click', () => {
         carrinho.style.display = 'block';
         body.style.overflowY = 'hidden';
-        mensagemTela.escurecerTela();
     });
 }
 abreMenu()
@@ -43,14 +42,3 @@ function mensagemCarrinho() {
     }
 }
 mensagemCarrinho();
-
-function clicouCarrinho() {
-    const iconeCarrinho = window.document.querySelectorAll('.sexta-carrinho');
-    const novoIconeCarrinho = removeElemento(iconeCarrinho, 0)
-    novoIconeCarrinho.forEach((element, indice) => {
-        element.addEventListener('click', () => {
-            return indice;
-        })
-    })
-}
-clicouCarrinho()
